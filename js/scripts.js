2491a3760bf2566f4885a872bac05deeee002586
Mega-117 <enricosaian@hotmail.it>
2021-10-07 15:42:09 +0200

    esercizio completato

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let nomeUtente = prompt("inserisci il tuo nome");
+let cognomeUtente = prompt("inserisci il tuo Cognome");
+let colorePreferito = prompt("inserisci il tuo colore preferito");
+
+console.log("Dati utente", nomeUtente, cognomeUtente, colorePreferito)
+
+alert("Ciao " + nomeUtente + " " + cognomeUtente + " ti piace il colore " + colorePreferito + " e sei l'utente n° 21");
