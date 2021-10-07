3fa05a95ab3b5373ea1b1247e011bb9b2aeea47f
Mega-117 <enricosaian@hotmail.it>
2021-10-07 17:44:54 +0200

    correzione

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,4 @@ let colorePreferito = prompt("inserisci il tuo colore preferito");
 
 console.log("Dati utente", nomeUtente, cognomeUtente, colorePreferito)
 
-alert("Ciao " + nomeUtente + " " + cognomeUtente + " ti piace il colore " + colorePreferito + " e sei l'utente n° 21");
+alert(nomeUtente +cognomeUtente +colorePreferito + 21);
